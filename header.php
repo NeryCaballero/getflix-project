d03954133a0fdc590171d3742f7df851a1668fa4
NeryCaballero <nerycaballero24@gmail.com>
2020-11-30 09:39:56 +0100

    test

---
+++ header.php
@@ -0,0 +1,36 @@
+<?php 
+
+// include_once 'header.php'; -----------> Paste this to every page 
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sweatish</title>
+    <link type="text/css" rel=stylesheet href="style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+</head>
+
+<body>
+
+    <!-- Nav Bar when NOT LOGGED IN -->
+    <div class="fixed-top">
+        <div class="collapse" id="navbarToggleExternalContent">
+          <div class="bg-dark p-4">
+            <h5 class="text-white h4">Collapsed content</h5>
+            <span class="text-muted">Toggleable via the navbar brand.</span>
+          </div>
+        </div>
+        <nav class="navbar navbar-dark bg-dark">
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+        </nav>
+      </div>
+    <!-- End of Nav Bar when NOT LOGGED IN -->
+    
+
+?>
---
+++ signup.php
@@ -0,0 +1,19 @@
+<?php 
+    include_once 'header.php';
+?>
+
+<section>
+    <h2>Sign Up</h2>
+    <form action="#" method="post">
+        
+
+    </form>     
+</section>
+
+
+
+
+
+<?php 
+    include_once 'footer.php';
+?>
