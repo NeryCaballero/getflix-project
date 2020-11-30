cb20fbb86b0a9d22c4be963d776e624d24f7e2b0
Juliette P <juliette.palluel@gmail.com>
2020-11-30 15:15:32 +0100

    database test

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -3,12 +3,14 @@
 $serverName ="localhost";
 $dbUsername ="root";
 $dbPassword ="";
-$dbName="sweatish_db"; // should match name in phpmyadmin
+$dbName="sweatish_db"; // Variable should match the name of our database in phpMyAdmin
 
-$conn =mysqli_connect($serverName,$dbUsername, $dbPassword,$dbName);// build-in function alllowing connexion btw Mysql and the client system
+$conn = mysqli_connect($serverName, $dbUsername, $dbPassword, $dbName);     // Built-in function allowing connexion between MySQL and the client system.
 
 
-//condition for connexion error handling
-if(!$conn){
+//condition for connection error handling
+// die() function prints a message and exits the current script.
 
+if(!$conn){                                                     // IF No Connection
+    die("Connection failed : " . mysqli_connect_error());       // echo "connection failed message + error details.
 }
---
+++ includes/functions.inc.php
@@ -0,0 +1,92 @@
+<?php
+
+// empty() PHP built-in function, Check whether a variable is empty. Also check whether the variable is set/declared:
+
+function emptyInputSignUp($name, $email, $username, $password, $passwordRepeat){                                    
+    $result;
+    if (empty($name) || empty($email) || empty($username) || empty($password) || empty($passwordRepeat)){           // IF 1 or more of our inputs is/are empty
+        $result = true;                                                                                             // emptyInputSignUp() result is true.
+    }
+    else {
+        $result = false;                                                                                            // otherwise, result is false.
+    }
+    return $result;
+}
+
+function invalidUid($username){
+    $result;
+    if (!preg_match("/^[a-zA-Z0-9]*$/", $username)){                // preg_match() PHP built-in function, returns whether a match was found in a string.
+        $result = true;                                             // if there is any characters other than a-z A-Z 0-9, we have an error.
+    }
+    else{
+        $result = false;                                            // otherwise all good.
+    }
+    return $result;
+}                                    
+
+function invalidEmail($email){
+    $result;
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)){                // filter_var() PHP built-in function, Filters a variable with a specified filter. 
+        $result = true;                                             // FILTER_VALIDATE_EMAIL: Validates whether the value is a valid e-mail address.
+    }
+    else{
+        $result = false;
+    }
+    return $result;
+}       
+
+function pwdMatch ($password, $passwordRepeat){
+    $result;
+    if($password !== $passwordRepeat){                              // IF password is NOT equal to passwordRepeat
+        $result = true;                                             // we have an error == result of pwdMartch() is true
+    }
+    else{
+        $result = false;                                            // otherwise, all good.
+    }
+    return $result;
+}
+
+
+function uidExists($conn, $username, $email){                                   // Syntax: SELECT column1, column2, ...   FROM table_name  WHERE condition; > WHERE : clause is used to extract only those records that fulfill a specified condition.the usersUid and usersEmail
+
+    $sql = "SELECT * FROM users WHERE usersUid = ? OR usersEmail = ?;";         // SELECT , * = ALL , FROM 'users' table , WHERE usersUid is ? (placeholder) OR usersEmail is ? (placeholder) > "userUid" and "usersEmail" are EXIATING columns in our users table.
+    $stmt = mysqli_stmt_init($conn);                                            // prepared statement prevents from  SQL injection, SQL injection is the placement of malicious code in SQL statements, via web page input. 
+    if(!my_sqli_stmt_prepare($stmt,$sql)) {                                     // mysqli_stmt_prepare() function prepares an SQL statement for execution => so this run the statement $stmt inside the database and see if it returns an error
+        header("location: ../signup.php?error=stmtfailed");                     // IF theres an error, redirect user to sign up page with an "statement failed" error.
+        exit();                                                                 // and Exit the script.
+    }
+
+    mysqli_stmt_bind_param($stmt, "ss", $username, $email);                     // Binds variables to a prepared statement as parameters takes 2 mandatories parameters: statement and the type (s for string)(one 's' per string)
+    mysqli_stmt_execute($stmt);                                                 // execute the prepared query
+
+    $resultData = mysqli_stmt_get_result($stmt);                                // Grab the data from the database
+
+    if($row = mysqli_fetch_assoc($resultData)) {                                // checks if username and email are already used/stored in database
+        return $row;                                                            // if no match, data is stored inside $row variable to store it after in database
+
+    } else {
+        $result = false;
+        return $result;
+    }
+
+    mysqli_stmt_close($stmt);                                                   // Close the statement
+}
+
+function createUser($conn, $name, $email, $username, $password){                                        // Syntax : INSERT INTO table_name (column1, column2, column3, ...) VALUES (value1, value2, value3, ...);
+
+    $sql = "INSERT INTO users(usersName, usersEmail, usersUid, usersPwd) VALUES(?, ?, ?, ?);";          // INSERT , INTO 'users' (COLUMN 1 2 3 4) (they must match exact name and order as in the table) VALUES (?, ?, ?, ?); (one '?' per value as placeholder).
+    $stmt = mysqli_stmt_init($conn);                                                                    // prepared statement prevents from  SQL injection, SQL injection is the placement of malicious code in SQL statements, via web page input. 
+    if(!my_sqli_stmt_prepare($stmt,$sql)) {                                                             // mysqli_stmt_prepare() function prepares an SQL statement for execution => so this run the statement $stmt inside the database and see if it returns an error
+        header("location: ../signup.php?error=stmtfailed");                                             // IF theres an error, redirect user to sign up page with an "statement failed" error.
+        exit();                                                                                         // and Exit the script.
+    }
+
+    $hashPassword = password_hash($password, PASSWORD_DEFAULT);                                         // password_hash() creates a new password hash using a strong one-way hashing algorithm. PASSWORD_DEFAULT - Use the bcrypt algorithm (default as of PHP 5.5.0). Note that this constant is designed to change over time as new and stronger algorithms are added to PHP
+    
+    mysqli_stmt_bind_param($stmt, "ssss", $name, $email, $username, $hashPassword);                     // Binds variables to a prepared statement as parameters takes 2 mandatories parameters: statement and the type (s for string)(one 's' per string)
+    mysqli_stmt_execute($stmt);                                                                         // execute the prepared query
+    mysqli_stmt_close($stmt);                                                                           // Close the statement
+    header("location: ../signup.php?error=none");                                                       // Redirect user to signup page , with no error message.
+    exit();                                                                                             // and Exit the script.
+
+}
